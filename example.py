709add8a3537dd81ef35ca0a3c47e3baad5c6c11
Leo Herrmann <leo.herrmann@physik.tu-berlin.de>
2019-05-29 14:29:30 +0300

    I've no idea what I did, but apparently it works now.

--- example.py
+++ example.py
@@ -8,7 +8,7 @@ def test_add():
 
 
 def subtract(a, b):
-    return a + b  # <--- fix this in step 8
+    return a - b  # <--- fix this in step 8
 
 def test_subtract():
     assert subtract(2, 3) == -1
